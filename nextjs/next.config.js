1d7a9dfb50e500b4a07e764aaf4721ae9fde5193
Zakey Faieq <zakeyf@gmail.com>
2023-07-28 20:57:24 -0400

    Setup dependencies for sqlite-wasm

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -4,7 +4,38 @@ const nextConfig = {
   experimental: {
     appDir: true,
     serverActions: true
-  }
-};
+  },
+  // see https://nextjs.org/docs/api-reference/next.config.js/headers
+  async headers() {
+    return [
+      {
+        source: '/function-calling',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/:slug*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      }
+    ]
+  },
+}
 
 module.exports = nextConfig;
